c401a6ec494524d25cb0b97d5c567614ad37d5a4
shihabLuminar <neethu@luminartechnolab.com>
2025-07-15 15:13:52 +0530

    quiz logic

--- lib/view/quiz_screen/quiz_screen.dart
+++ lib/view/quiz_screen/quiz_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_app/dummy_db.dart';
+import 'package:quiz_app/model/question.dart';
 import 'package:quiz_app/view/quiz_screen/widgets/options_card.dart';
 
 class QuizScreen extends StatefulWidget {
@@ -10,8 +11,13 @@ class QuizScreen extends StatefulWidget {
 }
 
 class _QuizScreenState extends State<QuizScreen> {
+  int questionIndex = 0;
+  int? clickedIndex;
+
   @override
   Widget build(BuildContext context) {
+    Question currentQuestion = DummyDb.questions[questionIndex];
+
     return Scaffold(
       appBar: AppBar(actions: [Text("1 / 10")]),
       body: Padding(
@@ -29,7 +35,7 @@ class _QuizScreenState extends State<QuizScreen> {
                 child: Center(
                   child: Text(
                     textAlign: TextAlign.center,
-                    DummyDb.questions[0].question,
+                    currentQuestion.question,
                   ),
                 ),
               ),
@@ -37,25 +43,65 @@ class _QuizScreenState extends State<QuizScreen> {
 
             Column(
               spacing: 16,
-              children: List.generate(4, (index) => OptionsCard()),
-            ),
-            Container(
-              width: double.infinity,
-              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-              decoration: BoxDecoration(
-                color: Colors.blue,
-
-                borderRadius: BorderRadius.circular(15),
+              children: List.generate(
+                currentQuestion.options.length,
+                (index) => OptionsCard(
+                  value: currentQuestion.options[index],
+                  color: _cardColor(index),
+                  onTap: () {
+                    if (clickedIndex == null) {
+                      clickedIndex = index;
+                      setState(() {});
+                    }
+                  },
+                ),
               ),
-              alignment: Alignment.center,
-              child: Text(
-                "Next",
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+            ),
+
+            Visibility(
+              visible: clickedIndex != null,
+              child: InkWell(
+                onTap: () {
+                  if (questionIndex < DummyDb.questions.length - 1) {
+                    questionIndex++;
+                    clickedIndex = null;
+                    setState(() {});
+                  }
+                },
+                child: Container(
+                  width: double.infinity,
+                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  alignment: Alignment.center,
+                  child: Text(
+                    "Next",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                  ),
+                ),
               ),
             ),
           ],
         ),
       ),
     );
+  }
+
+  Color _cardColor(int index) {
+    Question currentQuestion = DummyDb.questions[questionIndex];
+    if (clickedIndex != null && index == currentQuestion.ansIndex) {
+      return Colors.green;
+    }
+
+    if (index == clickedIndex) {
+      if (clickedIndex != currentQuestion.ansIndex) {
+        return Colors.red;
+      }
+    }
+
+    return Colors.grey;
   }
 }
--- lib/view/quiz_screen/widgets/options_card.dart
+++ lib/view/quiz_screen/widgets/options_card.dart
@@ -1,20 +1,31 @@
 import 'package:flutter/material.dart';
 
 class OptionsCard extends StatelessWidget {
-  const OptionsCard({super.key});
+  const OptionsCard({
+    super.key,
+    required this.value,
+    required this.color,
+    this.onTap,
+  });
+  final String value;
+  final Color color;
+  final void Function()? onTap;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-      decoration: BoxDecoration(
-        border: Border.all(color: Colors.grey),
-        borderRadius: BorderRadius.circular(15),
-      ),
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        decoration: BoxDecoration(
+          border: Border.all(color: color!),
+          borderRadius: BorderRadius.circular(15),
+        ),
 
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [Text("option "), Icon(Icons.circle_outlined)],
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [Text(value), Icon(Icons.circle_outlined)],
+        ),
       ),
     );
   }
